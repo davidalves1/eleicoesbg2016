4a4b320126511a69f841e8b429a35b48b6808068
davidalves1 <alves.david@outlook.com>
2016-09-22 00:21:54 -0300

    Remover diretiva

--- src/js/controllers/candidatosCtrl.js
+++ src/js/controllers/candidatosCtrl.js
@@ -16,7 +16,6 @@
 		$http.get('http://divulgacandcontas.tse.jus.br/divulga/rest/v1/candidatura/listar/2016/56154/2/11/candidatos')
 			.then(function(response) {
 				vm.candidatos = response.data.candidatos;
-				console.log(vm.candidatos);
 			}, function(error) {
 				console.log(error);
 			});
